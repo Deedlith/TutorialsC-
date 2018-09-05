1d7bd53cc0b9c1c9b31eccf4403cae6eafd595c4
Deedlithh <cindy.vuillaume@live.fr>
2018-09-05 11:35:04 +0200

    Projects + Configs

---
+++ TutoInventaire/Source/TutoInventaire/TutoInventaireGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TutoInventaireGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TUTOINVENTAIRE_API ATutoInventaireGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
